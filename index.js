316b10452b5105f97171631eaf7507df86b7501f
test2144 <84436967+test2144@users.noreply.github.com>
2022-03-08 02:41:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,33 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const TOKEN = process.env.BOT_TOKEN;
+require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+const TOKEN = process.env.TOKEN;
 
+const bot = new Client({ 
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS, //also enable in discord developer portal
+		Intents.FLAGS.GUILD_PRESENCES // This line is required to track presence data
+    ] 
+})
 bot.login(TOKEN);
 let game="за сервером";
 let stream="";
 bot.on('ready', () => {
-  bot.user.setActivity(game, { type: 'WATCHING', url: "https://www.youtube.com/watch?v=KeINtGHxfHU&ab_channel=14REGIONGAMING" });
+  bot.user.setActivity(game, { type: 'WATCHING'});
   console.info(`Logged in as ${bot.user.tag}!`);
 });
-bot.on('message', msg => {
-	const args = msg.content.trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
-	switch (command) {
+bot.on('presenceUpdate', async (oldPresence, newPresence) => {
+    let guild = oldPresence.guild;
+	const member = newPresence.member;
+    const activities = member.presence.activities[0];
+    const role = newPresence.guild.roles.cache.get('950377866342846494')
+	console.info(String(activities))
 	
-	case "play" :
-		game="";
-		var k=0;
-		for (k=0;k<args.length;k++){
-			game=game+" "+args[k];
-		}
-		playGame();
-		msg.channel.send('> Теперь я играю в '+game);
-		break;
-	case "watch" :
-		game="";
-		var k=0;
-		for (k=0;k<args.length;k++){
-			game=game+" "+args[k];
-		}
-		watchGame();
-		msg.channel.send('> Теперь я смотрю '+game);
-		break;
-	case "stream" :
-		game=args[0];
-		stream=args[1]
-		StreamingVideo();
-		msg.channel.send('> Теперь я стримлю '+args[0]+" "+stream);
-		break;
-	case "send" :
-		let channelId = args[0].replace(/[^0-9]/g, "");
-		let message="";
-		var k=0;
-		for (k=1;k<args.length;k++){
-			message=message+" "+args[k];
-		}
-		const channel = bot.channels.find(channel => channel.id === channelId)
-		if (channel==null){
-		msg.channel.send('> Искомый текстовый канал не найден, попробуйте еще раз');	
-		} else {
-		channel.send(""+message)
-		msg.channel.send('> Я отправил Ваше сообщение на '+args[0]+' с текстом:'+message);
-		console.info(message);
-		}
-		break;
-		
-}
+	switch (String(activities)) {
+	case "Counter-Strike: Global Offensive" :
+		return newPresence.member.roles.add(role);
+	case "Dota2" :
+		return newPresence.member.roles.add(role);
+	}
 });
-function playGame() {
-  bot.user.setActivity(game, {type: 'Playing'});
-}
-function watchGame() {
-  bot.user.setActivity(game, {type: 'WATCHING'});
-}
-function StreamingVideo() {
-   bot.user.setActivity(game, { type: "STREAMING", url: stream });
-}
