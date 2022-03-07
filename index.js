ced2b44cb12706b275744fc87badd0f438f05d79
test2144 <84436967+test2144@users.noreply.github.com>
2022-03-08 03:12:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-
-const { Client, Intents } = require('discord.js');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 const TOKEN = process.env.BOT_TOKEN;
 
-const bot = new Client({ 
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS, //also enable in discord developer portal
-		Intents.FLAGS.GUILD_PRESENCES // This line is required to track presence data
-    ] 
-})
 bot.login(TOKEN);
 let game="за сервером";
 let stream="";
@@ -17,17 +9,17 @@ bot.on('ready', () => {
   bot.user.setActivity(game, { type: 'WATCHING'});
   console.info(`Logged in as ${bot.user.tag}!`);
 });
-bot.on("presenceUpdate", async (oldPresence, newPresence) => {
+
+bot.on('presenceUpdate', async (oldPresence, newPresence) => {
     const roleD2 = newPresence.guild.roles.cache.get('949641980433014837')
     const roleG = newPresence.guild.roles.cache.get('950231538090512415')
-	const member = newPresence.member;
-    const activities = member.presence.activities[0];
-	console.info(String(activities))
-	
-	switch (String(activities)) {
+    const member = newPresence.member
+    const activities = member.user.presence.activities[0];
+  
+    switch (String(activities)) {
 	case "Genshin Impact" :
 		return newPresence.member.roles.add(role);
 	case "Dota 2" :
 		return newPresence.member.roles.add(roleD2);
 	}
-});
+})
